ca27eeb8168a0b7f2765095e2e2bd51fd6fb9795
danilo velasquez <cristian.velasquez@correounivalle.edu.co>
2021-01-19 08:54:58 -0500

    correcciones rutas imagenes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,10 @@
         <h1 class="titulo">BIENVENIDOS!!</h2>
         <div class="slider">
             <ul>
-                <li><img src="{{asset('img/carru1.PNG')}}" alt="carru1"></li>
-                <li><img src="{{asset('img/carru2.PNG')}}" alt="carru2"></li>
-                <li><img src="{{asset('img/carru3.jpg')}}" alt="carru3"></li>
-                <li><img src="{{asset('img/carru4.png')}}" alt="carru4"></li>
+                <li><img src="{{asset('./img/carru1.png')}}" alt="carru1"></li>
+                <li><img src="{{asset('./img/carru2.PNG')}}" alt="carru2"></li>
+                <li><img src="{{asset('./img/carru3.jpg')}}" alt="carru3"></li>
+                <li><img src="{{asset('./img/carru4.png')}}" alt="carru4"></li>
             </ul>
         </div>
     </section>
